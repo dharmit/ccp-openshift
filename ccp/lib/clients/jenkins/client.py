6b13858eff515096c2ee1acd371a33aa6607487f
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2018-08-20 16:27:17 +0530

    Adding more docs and renaming functions.

--- ccp/lib/clients/jenkins/client.py
+++ ccp/lib/clients/jenkins/client.py
@@ -19,34 +19,58 @@ class OpenshiftJenkinsWorkflowAPIClient(OpenshiftJenkinsBaseAPIClient):
             port=port
         )
 
-    def get_runs(self, ordered_project_list):
+    def get_build_runs(self, ordered_jobs_list):
+        """
+        Gets the build runs of specified job/subjob from API server
+        :param ordered_jobs_list: The ordered list of jobs, with parents,
+        followed by children
+        :type ordered_jobs_list list
+        :return: The Response returned by api server.
+        """
         return self._query(
             "{}/wfapi/runs".format(
                 self._jenkins_jobs_from_jobs_ordered_list(
-                    ordered_project_list
+                    ordered_jobs_list
                 )
             )
         )
 
     def describe_build_run(
-            self, ordered_projects_list, build_number
+            self, ordered_jobs_list, build_number
     ):
+        """
+        Describes a particular build run of job/subjob from API server
+        :param ordered_jobs_list: The ordered list of jobs, with parents,
+        followed by children
+        :type ordered_jobs_list list
+        :param build_number: The number of build to describe.
+        :type build_number str
+        :return: The Response returned by the API server.
+        """
         return self._query(
             "{}/{}/wfapi/describe".format(
                 self._jenkins_jobs_from_jobs_ordered_list(
-                    ordered_projects_list
+                    ordered_jobs_list
                 ),
                 build_number
             )
         )
 
     def describe_execution_node(
-            self, ordered_projects_list, build_number, node_number
+            self, ordered_jobs_list, build_number, node_number
     ):
+        """
+        Describes an execution node for job/subjob from API server
+        :param ordered_jobs_list: The ordered list of jobs, with parents,
+        followed by children
+        :param build_number:The number of the build to describe.
+        :param node_number: The number of the node to describe
+        :return: The Response from the api server
+        """
         return self._query(
             "{}/{}/execution/node/{}/wfapi/describe".format(
                 self._jenkins_jobs_from_jobs_ordered_list(
-                    ordered_projects_list
+                    ordered_jobs_list
                 ),
                 build_number,
                 node_number
@@ -54,12 +78,12 @@ class OpenshiftJenkinsWorkflowAPIClient(OpenshiftJenkinsBaseAPIClient):
         )
 
     def get_logs_of_execution_node(
-            self, ordered_projects_list, build_number, node_number
+            self, ordered_jobs_list, build_number, node_number
     ):
         return self._query(
             "{}/{}/execution/node/{}/wfapi/log".format(
                 self._jenkins_jobs_from_jobs_ordered_list(
-                    ordered_projects_list
+                    ordered_jobs_list
                 ),
                 build_number,
                 node_number
