4399ab0764c1c9964a7ed78ced5468e1565cb37c
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2018-08-20 15:44:47 +0530

    Adding functions to query important endpoints of jenkins wf api.

--- ccp/lib/clients/jenkins/client.py
+++ ccp/lib/clients/jenkins/client.py
@@ -2,6 +2,9 @@ from ccp.lib.clients.jenkins.base import OpenshiftJenkinsBaseAPIClient
 
 
 class OpenshiftJenkinsWorkflowAPIClient(OpenshiftJenkinsBaseAPIClient):
+    """
+    Helps query the jenkins workflow api endpoints.
+    """
 
     def __init__(
             self,
@@ -13,11 +16,49 @@ class OpenshiftJenkinsWorkflowAPIClient(OpenshiftJenkinsBaseAPIClient):
             port=port
         )
 
-    def describe_build_run(self, project, build_number, super_project=None):
+    def get_runs(self, ordered_project_list):
         return self._query(
-            "{}/job{}{}".format(
-                "/" + super_project + "/job" if super_project else "",
-                "/" + project,
-                "/" + build_number
+            "{}/wfapi/runs".format(
+                self._jenkins_jobs_from_jobs_ordered_list(
+                    ordered_project_list
+                )
+            )
+        )
+
+    def describe_build_run(
+            self, ordered_projects_list, build_number
+    ):
+        return self._query(
+            "{}/{}/wfapi/describe".format(
+                self._jenkins_jobs_from_jobs_ordered_list(
+                    ordered_projects_list
+                ),
+                build_number
+            )
+        )
+
+    def describe_execution_node(
+            self, ordered_projects_list, build_number, node_number
+    ):
+        return self._query(
+            "{}/{}/execution/node/{}/wfapi/describe".format(
+                self._jenkins_jobs_from_jobs_ordered_list(
+                    ordered_projects_list
+                ),
+                build_number,
+                node_number
+            )
+        )
+
+    def get_logs_of_execution_node(
+            self, ordered_projects_list, build_number, node_number
+    ):
+        return self._query(
+            "{}/{}/execution/node/{}/wfapi/log".format(
+                self._jenkins_jobs_from_jobs_ordered_list(
+                    ordered_projects_list
+                ),
+                build_number,
+                node_number
             )
         )
