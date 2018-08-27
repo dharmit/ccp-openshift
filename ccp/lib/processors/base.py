d6f7269c85971266bc0502a994b626324b253795
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2018-08-27 14:43:50 +0530

    JSON processors can choose to parse as literal if badjson is set.

--- ccp/lib/processors/base.py
+++ ccp/lib/processors/base.py
@@ -2,7 +2,7 @@
 This file contains base classes of query processor.
 """
 
-from ccp.lib.utils import json_to_python
+from ccp.lib.utils import json_to_python, parse_literals
 
 
 class QueryProcessor(object):
@@ -17,14 +17,20 @@ class JSONQueryProcessor(QueryProcessor):
     Acts as base of JSONQueryProcessor
     """
 
-    def get_data_from_response(self, response):
+    def get_data_from_response(self, response, bad_json=False):
         """
-        Extracts json data from a requests response object
+        Extracts data from a requests response object
         :param response: The response from which we need to extract information
         :type response requests.response
-        :return: response result as json, if it can get it, Else None
+        :param bad_json: Default False: If true, uses ast eval instead of json
+        load
+        :type bad_json bool
+        :return: response result in pythonic form, if it can get it, Else None
         """
         data = None
         if response:
-            data = json_to_python(response.text)
+            if not  bad_json:
+                data = json_to_python(response.text)
+            else:
+                data = parse_literals(response.text)
         return data
